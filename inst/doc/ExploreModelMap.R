785b2b962e59709cf0ca488c7b80f588c78ed5c9
Elizabeth Freeman <eafreeman@fs.fed.us>
2022-08-18 17:30:10 +0000

    version 3.4.0.2

--- inst/doc/ExploreModelMap.R
+++ inst/doc/ExploreModelMap.R
@@ -1,5 +1,4 @@
 ### R code from vignette source 'ExploreModelMap.Rnw'
-### Encoding: ISO8859-1
 
 ###################################################
 ### code chunk number 1: ExSetup set options
--- inst/doc/VModelMap.R
+++ inst/doc/VModelMap.R
@@ -1,5 +1,4 @@
 ### R code from vignette source 'VModelMap.Rnw'
-### Encoding: ISO8859-1
 
 ###################################################
 ### code chunk number 1: Ex1 set options
